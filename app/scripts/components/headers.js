ad7823bea5c830fc0e65c84575ded67168e94fa1
Padraig Fl <padraig_f@hotmail.com>
2016-01-22 12:28:03 -0800

    added colors, rsolved navbar flow issues, changed some general stylings, overflow issues

--- app/scripts/components/headers.js
+++ app/scripts/components/headers.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-  $(".navbar-collapse").on("hide.bs.collapse", function(){
-    $("#menu-collapse").html('<span class="glyphicon glyphicon-menu-up"></span>');
-  });
-  $(".navbar-collapse").on("show.bs.collapse", function(){
-    $("#menu-collapse").html('<span class="glyphicon glyphicon-menu-down"></span> ');
-  });
-});
+//
