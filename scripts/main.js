457f213c6056b712357c71bfaf9bedce3c5a5c7b
Joshua Pekera <joshuapekera@me.com>
2016-02-12 11:14:47 -0800

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+$(".navbar-collapse").on("shown.bs.collapse",function(){$(".glyph-flip").addClass("glyphicon-chevron-up").removeClass("glyphicon-chevron-down")}),$(".navbar-collapse").on("hidden.bs.collapse",function(){$(".glyph-flip").addClass("glyphicon-chevron-down").removeClass("glyphicon-chevron-up")});
